8f57e75dc3ad5cb2cb88cacc33c26ea1b794add7
austingriffith94 <austgriffia@gmail.com>
2017-12-02 14:53:30 -0500

    added download 8-k function in class

--- event_study.py
+++ event_study.py
@@ -16,6 +16,7 @@ from shutil import move
 import fileinput
 import os
 import io
+from bs4 import BeautifulSoup as beaut
 
 # create directory for data files
 if os.path.exists('data') == False:
@@ -67,12 +68,13 @@ class sec:
                         f.write(link)
 
     def read_index(self):
-        raw = "https://www.sec.gov/Archives/"
-        file = open('data/sec.csv','w')
+        random.seed(903353429)
+        rows_list = []
 
         for t in self.T:
             for q in self.Q:
                 index = 'data/master'+t+q+'.idx'
+                raw = "https://www.sec.gov/Archives/"
 
                 with open(index) as f:
                     line = random.sample(f.readlines(),self.n)
@@ -83,24 +85,31 @@ class sec:
                     date = s[3]
                     dr = s[4].split('/')
                     name = 'data/'+dr[3].rstrip('\n')
-                    url = raw+s[4]
-                    row = [cik,date,url]
+                    url = raw+s[4].rstrip('\n')
+                    row = {'cik':cik,'date':date,
+                        'name':name,'url':url}
+                    rows_list.append(row)
 
-                    write = csv.writer(file, delimiter=',')
-                    write.writerow(row)
-        file.close()
+        res = pd.DataFrame(rows_list)
+        res.to_csv('sec.csv')
 
     def dl_forms(self):
-        pass
+        pull = pd.read_csv('sec.csv')
+        for i in range(1, pull.shape[0]):
+            form = requests.get(pull['url'][i])
+            soup = beaut(form.text, 'lxml').text
+            file = open('data/'+pull['cik'][i].astype(str)+' '+pull['date'][i]+'.txt','w', encoding='utf8')
+            file.write(soup)
+            file.close
 
 
 # years to look at
 t0 = 1995
-t1 = 1995
+t1 = 2016
 n = 100
 gov = sec(t0,t1,n)
 gov.pull_index()
 gov.clean_index()
 gov.read_index()
 # gov.del_index()
-
+# gov.dl.forms()
